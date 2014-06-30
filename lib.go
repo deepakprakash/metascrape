240b3f7e3777c07178630573d685f42bf8ff488b
deepakprakash <dp@deepakprakash.com>
2014-06-30 12:12:34 +0200

    Use the SoundCloudHandler in the Default scraper

--- lib.go
+++ lib.go
@@ -73,6 +73,7 @@ func Default() *MetaScraper {
 	scraper.Use(contrib.GenericHandler)
 	scraper.Use(contrib.EtsyProductHandler)
 	scraper.Use(contrib.YouTubeVideoHandler)
+	scraper.Use(contrib.SoundCloudAudioHandler)
 
 	return scraper
 }
